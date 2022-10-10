2cec282d36ef899709a36c26013b2a2418d8c8c0
Tushar Sureka <85789860+tusharsureka@users.noreply.github.com>
2022-10-11 01:19:30 +0530

    Update Quicksort.cpp

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -1,65 +1,70 @@
 
-#include <bits/stdc++.h> 
-using namespace std; 
 
+// Quick sort in C++
 
-void swap(int* a, int* b) 
-{ 
-	int t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
+#include <iostream>
+using namespace std;
 
+// function to swap elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 
-int partition (int arr[], int low, int high) 
-{ 
-	int pivot = arr[high]; 
-	int i = (low - 1);  
+// function to print the array
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
 
-	for (int j = low; j <= high - 1; j++) 
-	{ 
-		 
-		if (arr[j] < pivot) 
-		{ 
-			i++;  
-			swap(&arr[i], &arr[j]); 
-		} 
-	} 
-	swap(&arr[i + 1], &arr[high]); 
-	return (i + 1); 
-} 
+// function to rearrange array (find the partition point)
+int partition(int array[], int low, int high) {
+	
+  int pivot = array[high];// select the rightmost element as pivot
+  int i = (low - 1);// pointer for greater element
 
+  
+  for (int j = low; j < high; j++) {// traverse each element of the array
+    if (array[j] <= pivot) {  // compare them with the pivot
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      swap(&array[i], &array[j]);// swap element at i with element at j
+    }
+  }
+  swap(&array[i + 1], &array[high]); // swap pivot with the greater element at i
+  
+  return (i + 1);  // return the partition point
+}
 
-void quickSort(int arr[], int low, int high) 
-{ 
-	if (low < high) 
-	{ 
-		
-		int pi = partition(arr, low, high); 
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+      
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on righ of pivot
+    int pi = partition(array, low, high);
+	  
+    quickSort(array, low, pi - 1);// recursive call on the left of pivot
+    quickSort(array, pi + 1, high);// recursive call on the right of pivot
+  }
+}
 
-		
-		quickSort(arr, low, pi - 1); 
-		quickSort(arr, pi + 1, high); 
-	} 
-} 
-
-
-void printArray(int arr[], int size) 
-{ 
-	int i; 
-	for (i = 0; i < size; i++) 
-		cout << arr[i] << " "; 
-	cout << endl; 
-} 
-
-//driver function
-int main() 
-{ 
-	int arr[] = {10, 7, 8, 9, 1, 5}; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
-	quickSort(arr, 0, n - 1); 
-	cout << "Sorted array: "; 
-	printArray(arr, n); 
-	return 0; 
-} 
+// Driver code
+int main() {
+  int data[] = {8, 7, 6, 1, 0, 9, 2};
+  int n = sizeof(data) / sizeof(data[0]);
+  
+  cout << "Unsorted Array: \n";
+  printArray(data, n);
+ 
+  quickSort(data, 0, n - 1); // perform quicksort on data
+  
+  cout << "Sorted array in ascending order: \n";
+  printArray(data, n);
+}
 // END
